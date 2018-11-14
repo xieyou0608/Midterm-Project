aadbd69c1f47382c7c64a7a9777b8884d0737779
peiwenhuang27 <45028573+peiwenhuang27@users.noreply.github.com>
2018-11-14 15:51:02 +0800

    Add files via upload
    main function: cin

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//  pd midterm project
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0, m = 0, w = 0, d = 0;
+    cin >> n >> m >> w >> d;
+    int* X = new int [m];
+    int* Y = new int [m];
+    int* R = new int [m];
+    int* P = new int [m];
+    for(int i = 0; i < m; i++)
+        cin >> X[i];
+    for(int i = 0; i < m; i++)
+        cin >> Y[i];
+    for(int i = 0; i < m; i++)
+        cin >> R[i];
+    for(int i = 0; i < m; i++)
+        cin >> P[i];
+    
+    //delete statements
+    delete [] X;
+    delete [] Y;
+    delete [] R;
+    delete [] P;
+    return 0;
+}
